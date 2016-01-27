ff5207f11cbbdae6666e8810711b0b185256f8f4
Tom W <tom.whiston@gmail.com>
2016-01-27 18:11:32 +0100

    added drush logger

--- src/DrushLogger.php
+++ src/DrushLogger.php
@@ -12,8 +12,22 @@ use Psr\Log\AbstractLogger;
 
 
 class Drush extends AbstractLogger {
+  
   public function log($level, $message, array $context = array()) {
     // TODO: Implement log() method.
+    $message = dt($message, $context);
+
+    //build the type
+    /**
+    *   The type of message to be logged. Common types are 'warning', 'error', 'success' and 'notice'.
+    *   A type of 'failed' can also be supplied to flag as an 'error'.
+    *   A type of 'ok' or 'completed' can also be supplied to flag as a 'success'.
+    *   If you want your log messages to print to screen without the user entering
+    *   a -v or --verbose flag, use type 'ok', this prints log messages out to
+    *   STDERR, which prints to screen (unless you have redirected it). All other
+    *   types of messages will be assumed to be notices.
+     * */
+    drush_log($message, $level);
   }
 
 
