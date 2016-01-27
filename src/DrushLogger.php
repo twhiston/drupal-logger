06c0533d2aa16e43ea07cdeb714fd57cdd4ceca3
Tom W <tom.whiston@gmail.com>
2016-01-27 22:46:51 +0100

    Better drush logging

--- src/DrushLogger.php
+++ src/DrushLogger.php
@@ -14,21 +14,21 @@ use Psr\Log\AbstractLogger;
 class DrushLogger extends AbstractLogger {
 
   public function log($level, $message, array $context = array()) {
-    // TODO: Implement log() method.
     $message = dt($message, $context);
 
-    //build the type
     /**
-    *   The type of message to be logged. Common types are 'warning', 'error', 'success' and 'notice'.
-    *   A type of 'failed' can also be supplied to flag as an 'error'.
-    *   A type of 'ok' or 'completed' can also be supplied to flag as a 'success'.
-    *   If you want your log messages to print to screen without the user entering
-    *   a -v or --verbose flag, use type 'ok', this prints log messages out to
-    *   STDERR, which prints to screen (unless you have redirected it). All other
-    *   types of messages will be assumed to be notices.
+     *   The type of message to be logged. Common types are 'warning', 'error', 'success' and 'notice'.
+     *   A type of 'failed' can also be supplied to flag as an 'error'.
+     *   A type of 'ok' or 'completed' can also be supplied to flag as a 'success'.
+     *   If you want your log messages to print to screen without the user entering
+     *   a -v or --verbose flag, use type 'ok', this prints log messages out to
+     *   STDERR, which prints to screen (unless you have redirected it). All other
+     *   types of messages will be assumed to be notices.
      * */
+    if(strcmp($level,'notice') == 0){ $level = 'ok';} //Make the level notice equal drush OK so it is printed
+
+
     drush_log($message, $level);
   }
-
 
 }
